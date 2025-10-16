bc7c0ea9646f7a73d319a97f573206353a7e28bf
RobbyR98 <robbyrunge@yahoo.com>
2025-10-16 21:59:22 +0200

    feat: update BoardAdmin to display member count and refine search and filter options

--- boards_app/admin.py
+++ boards_app/admin.py
@@ -5,6 +5,11 @@ from .models import Board
 
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-    list_display = ['title', 'owner', 'created_at', 'updated_at']
-    search_fields = ['title', 'description']
-    date_hierarchy = 'created_at'
+    list_display = ['title', 'owner', 'get_member_count']
+    search_fields = ['title']
+    list_filter = ['owner']
+    filter_horizontal = ['members']
+    
+    def get_member_count(self, obj):
+        return obj.members.count()
+    get_member_count.short_description = 'Members'
