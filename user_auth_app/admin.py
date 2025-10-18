baf8bcd9729086187bebe3eee7e1cb36a1b3b028
RobbyR98 <robbyrunge@yahoo.com>
2025-10-18 21:49:59 +0200

    feat: implement CustomUserAdmin for User model

--- user_auth_app/admin.py
+++ user_auth_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.unregister(User)
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'id', 'email', 'is_staff', 'date_joined', 'last_login')
+    list_filter = ('is_staff', 'is_superuser')
+    search_fields = ('username', 'email',)
